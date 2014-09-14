b6eb5352c9b8786e0570b6774d8ab217edc70d55
Puru Dahal <pdahal@me.com>
2014-09-14 01:54:12 -0700

    Spike on traitify to show the assessment and and results using their widgets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,13 @@
 //= require_tree .
 //= require traitify_api_1.0.0.js
 //= require traitify_widget_2.0.0.js
+//= require traitify_results_1.0.0.js
+
+console.log('loads last')
+Traitify.setPublicKey("8kvtmdpdp6b2n2nep5a60oc1f9");
+  Traitify.setHost("https://api-sandbox.traitify.com");
+  Traitify.setVersion("v1");
+  var assessmentId = '4094248c-7eca-404a-886d-23e9ad8aa030';
+  Traitify.ui.slideDeck(assessmentId, ".assessment", function(data) {
+    Traitify.ui.resultsProp(assessmentId, ".assessment", {showTraits: true});
+  });
