7027132ac43457d9b4f0b40396f7b5f3c9979240
Brian <bajunio@gmail.com>
2014-09-14 14:21:28 -0700

    add assessment IDs for placement to guides

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,20 @@ console.log('loads last')
 Traitify.setPublicKey("8kvtmdpdp6b2n2nep5a60oc1f9");
   Traitify.setHost("https://api-sandbox.traitify.com");
   Traitify.setVersion("v1");
-  var assessmentId = '4094248c-7eca-404a-886d-23e9ad8aa030';
+  var assessmentId = '3448fbdc-0dcd-4040-a137-d0b85a85f3f4';
   Traitify.ui.slideDeck(assessmentId, ".assessment", function(data) {
     Traitify.ui.resultsProp(assessmentId, ".assessment", {showTraits: true});
   });
+
+
+// fef5867a-a4a6-43e0-aa4e-94711ae71c51  - complete
+
+// 9c848797-25bd-4b4a-b5cb-b81052a52eb1 - complete
+
+// f46c70ef-8b36-49c9-bf1e-6b3fcba2f0ad - complete
+
+// 311d902f-24a9-4944-91da-dc3066e12b28 - complete
+
+// 3448fbdc-0dcd-4040-a137-d0b85a85f3f4 - complete
+
+// 195c1416-362a-4f88-9641-d9494b92e086 - complete (all yes, 100 on all)
