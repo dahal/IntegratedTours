a4313b4c89c9b55735f4c25cefb99a8855c21d70
Roman Gurovich <romangurovich@gmail.com>
2014-09-13 22:22:44 -0700

    sorting

--- app/assets/javascripts/collections/guides.js
+++ app/assets/javascripts/collections/guides.js
@@ -1,3 +1,18 @@
 App.Collections.Guides = Backbone.Collection.extend({
-  model: App.Models.Guide
+    url: 'guides',
+    model: App.Models.Guide,
+    comparator: function(a, b) {
+        a = a.get(this.sort_key);
+        b = b.get(this.sort_key);
+        return a > b ?  1
+             : a < b ? -1
+             :          0;
+    },
+    changeSort: function (sortProperty) {
+        this.sortKey = sortProperty; 
+        this.sort();
+    },
+    initialize: function () {
+        this.sortKey = 'name'; 
+    }
 });
--- app/assets/javascripts/views/guides_index.js
+++ app/assets/javascripts/views/guides_index.js
@@ -3,5 +3,6 @@ IntegratedTours.Views.GuidesIndex = Backbone.View.extend({
     template: JST['guides/index'],
     initialize: function(args) {
         var args = args || {};
+        this.listenTo(this.collection, 'reset sort', this.render);
     }
 });
